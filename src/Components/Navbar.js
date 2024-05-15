e8a8a3209c1731ba59ce5e52b300b0a0aa51fbed
AntonyRoosvelt <antony.roosveltp@gmail.com>
2024-05-15 23:59:30 +0530

    Update pr

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+function Navbar()
+{
+
+  return(
+    <nav>
+      <h1>Perfumy</h1>
+      <ul>
+        <li>Home |</li>
+        <li>Products |</li>
+        <li>About |</li>
+        <li>Conduct |</li>
+      </ul>
+    </nav>
+  );
+
+}
+
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client"
+import "./style.css"
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+// import one from "./assets/images/one.jpg"
+// import two from "./assets/images/two.jpg" 
+// import three from "./assets/images/three.jpg"
+
+
+
+
+
+
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <App></App>
+)
